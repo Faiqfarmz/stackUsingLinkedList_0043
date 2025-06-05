c36d1245b975f372d41ee5cf9170bfe696f43384
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-05 11:20:04 +0700

    void pop

--- stack.cpp
+++ stack.cpp
@@ -73,44 +73,4 @@ class Stack
     }
 };
 
-int main()
-{
-    Stack stack;
-
-    int choice = 0;
-    int value;
-
-    while (choice != 4)
-    {
-        cout << "1. Push\n";
-        cout << "2. Pop\n";
-        cout << "3. Peek\n";
-        cout << "4. Exit\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice)
-        {
-            case 1:
-                cout << "Enter the value to push: ";
-                cin >> value;
-                stack.push(value); // push the entered onto the stack
-                break;
-            case 2:
-                stack.pop(); // Pop the topmost elemetn from the stack
-                break;
-            case 3:
-                stack.peek();
-                break;
-            case 4:
-                cout << "Exiting program. " << endl;
-                break;
-                default:
-                cout << "Invalid choice. Try again." << endl;
-                break;
-        }
-        cout << endl;
-    }
-    return 0;
-}
 
