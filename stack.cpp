25944730bf29b06c03538a408040ebc76b386366
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-05 11:06:42 +0700

    case 1

--- stack.cpp
+++ stack.cpp
@@ -89,6 +89,13 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
 
-        
+        switch (choice)
+        {
+            case 1:
+                cout << "Enter the value to push: ";
+                cin >> value;
+                strack.push(value); // push the entered onto the stack
+                break;
+        }
     }
 }
